787fb191bf2f189f4c01ac18ce57c8c06d68e47f
yassbarona <yasserbarona@gmail.com>
2022-02-24 16:38:47 +0100

    creating CHM file

--- utils/search.py
+++ utils/search.py
@@ -8,6 +8,9 @@ import os
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.io as pio
+pio.renderers.default = "browser"
+
 
 
 class Search:
@@ -45,38 +48,66 @@ class Search:
         transform = osr.CoordinateTransformation(src_srs, tgt_srs)
         x,y,z = transform.TransformPoint(lat,lon)
         lambert_coo.append([x,y])
-        self.tif(lambert_coo)
+        self.tif_dsm(lambert_coo)
+        self.tif_dtm(lambert_coo)
         
-    def tif(self, lambert_coo):
-        folder_path = os.path.abspath("/home/becode2/data/GeoTIFF/")
+    def tif_dsm(self, lambert_coo):
+        folder_path = os.path.abspath("/home/becode2/data/GeoTIFF_DSM/")
         for path, dirs, files in os.walk(folder_path):
             for filename in files:
                 tif = f'{folder_path}/{filename}'
-                possible_ds = rasterio.open(tif)
-                b_list = possible_ds.bounds
+                possible_dsm_ds = rasterio.open(tif)
+                b_list = possible_dsm_ds.bounds
                 #print(f'{possible_ds}, {int(b_list[0])},{int(b_list[1])},{int(lambert_coo[0][0])},{int(lambert_coo[0][1])},')
                 if int(b_list[0]) <= int(lambert_coo[0][0]) <= int(b_list[2]) and int(b_list[1]) <= int(lambert_coo[0][1]) <= int(b_list[3]) :
-                    ds = possible_ds
-                    print(ds.name)
-                    self.tif_clipping(ds,lambert_coo)
+                    ds_dsm = possible_dsm_ds
+                    self.tif_dsm_clipping(ds,lambert_coo)
+    
+    def tif_dtm(self, lambert_coo):
+            folder_path = os.path.abspath("/home/becode2/data/GeoTIFF_DTM/")
+            for path, dirs, files in os.walk(folder_path):
+                for filename in files:
+                    tif = f'{folder_path}/{filename}'
+                    possible_dtm_ds = rasterio.open(tif)
+                    b_list = possible_dtm_ds.bounds
+                    #print(f'{possible_ds}, {int(b_list[0])},{int(b_list[1])},{int(lambert_coo[0][0])},{int(lambert_coo[0][1])},')
+                    if int(b_list[0]) <= int(lambert_coo[0][0]) <= int(b_list[2]) and int(b_list[1]) <= int(lambert_coo[0][1]) <= int(b_list[3]) :
+                        ds_dtm = possible_dtm_ds
+                        self.tif_dsm_clipping(ds,lambert_coo)
+
+    def boundries(self, )
+         xmin = lambert_coo[0][0] - 50
+        xmax = lambert_coo[0][0] + 50
+        ymin = lambert_coo[0][1] - 50
+        ymax = lambert_coo[0][1] + 50
+        bbox = (xmin,ymax,xmax,ymin)           
+
+    def tif_dsm_clipping(self,ds_dsm,lambert_coo):
+        
+        print(bbox, lambert_coo[0][1])
+        gdal.Translate('new.tif', ds.name, projWin = bbox)
+        ds2 = rasterio.open('new.tif')
+        self.final_plot(ds2)
+
+    
                     
 
-    def tif_clipping(self,ds,lambert_coo):
-        xmin = lambert_coo[0][0] - 20
-        xmax = lambert_coo[0][0] + 20
-        ymax = lambert_coo[0][1] + 20
-        ymin = lambert_coo[0][1] - 20
+    def tif_dtm_clipping(self,ds,lambert_coo):
+        xmin = lambert_coo[0][0] - 50
+        xmax = lambert_coo[0][0] + 50
+        ymin = lambert_coo[0][1] - 50
+        ymax = lambert_coo[0][1] + 50
         bbox = (xmin,ymax,xmax,ymin)
         print(bbox, lambert_coo[0][1])
-        ds2 = gdal.Translate('new.tif', ds.name, projWin = bbox)
-        ds3 = rasterio.open('new.tif')
-        self.final_plot(ds3)
-        
+        gdal.Translate('new.tif', ds.name, projWin = bbox)
+        ds2 = rasterio.open('new.tif')
+        self.final_plot(ds2)
 
     def final_plot(self,ds3):
         array = ds3.read(1)
         df = pd.DataFrame(data=array)
         fig = go.Figure(data=[go.Surface(z=df.values)])
-        fig.update_layout(title='Pyiterator', autosize=False, width=1000, height=1000, margin=dict(l=65, r=20, b=65, t=90))
+        fig.update_layout(title='Pyiterator', autosize=False, width=1000, height=1000, margin=dict(l=65, r=50, b=65, t=90))
         fig.show()
+        ds3.close()
 Search()
