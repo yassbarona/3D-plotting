0a41a442b4b81a99e64986cd4da9f65917bcb9f8
yassbarona <yasserbarona@gmail.com>
2022-02-25 13:22:25 +0100

    organizing the code

--- utils/search.py
+++ utils/search.py
@@ -16,10 +16,10 @@ pio.renderers.default = "browser"
 class Search:
 
     nom = Nominatim(user_agent="Mon_Application")
-   
+    lambert_coo = []
 
     def __init__(self):
-        address = input('Enter address ')
+        address = input('Enter address--->')
         result = self.nom.geocode(address)
         try:
             confirmation = input(f'\n Is this the address you are looking for? \n”{result.raw["display_name"]}" \n Yes/Y No/N --> ')
@@ -34,80 +34,73 @@ class Search:
                 print('Invalid answer. Please try again!')
                 Search()
         except Exception as e:
-            print('Somethign went wrong. Verify that address is corrrect')
+            print('Somethign went wrong, please try again. Verify that inputs are corrrect')
             print('ex',e)
-            
+            Search()
 
     def coordinates(self, lat, lon):
         gps_coo = [lat, lon]
-        lambert_coo = []
         tgt_srs= osr.SpatialReference()
         tgt_srs.ImportFromEPSG(31370)
         src_srs= osr.SpatialReference()
         src_srs.ImportFromEPSG(4326)
         transform = osr.CoordinateTransformation(src_srs, tgt_srs)
         x,y,z = transform.TransformPoint(lat,lon)
-        lambert_coo.append([x,y])
-        self.tif_dsm(lambert_coo)
-        self.tif_dtm(lambert_coo)
+        self.lambert_coo.append([x,y])
+        self.tif_files()
         
-    def tif_dsm(self, lambert_coo):
+
+    def boundries(self):
+        n = int(input('\n Please enter the desired range in m² --->'))
+        if n != int:
+            print("Invalid answer. Please try again!")
+            self.boundries()
+        elif n > 500:
+            print('Sorry, that range is too large, try a smaller one')
+            self.boundries()
+        else:
+            xmin = self.lambert_coo[0][0] - n
+            xmax = self.lambert_coo[0][0] + n
+            ymin = self.lambert_coo[0][1] - n
+            ymax = self.lambert_coo[0][1] + n
+            bbox = [xmin,ymax,xmax,ymin]
+            return(bbox) 
+        
+        
+    def tif_files(self):
+        lambert_coo = self.lambert_coo 
         folder_path = os.path.abspath("/home/becode2/data/GeoTIFF_DSM/")
         for path, dirs, files in os.walk(folder_path):
             for filename in files:
-                tif = f'{folder_path}/{filename}'
-                possible_dsm_ds = rasterio.open(tif)
+                tif_dsm = f'{folder_path}/{filename}'
+                possible_dsm_ds = rasterio.open(tif_dsm)
                 b_list = possible_dsm_ds.bounds
-                #print(f'{possible_ds}, {int(b_list[0])},{int(b_list[1])},{int(lambert_coo[0][0])},{int(lambert_coo[0][1])},')
+                #print(f'{possible_dsm_ds.bounds}, {int(b_list[0])},{int(b_list[3])},{int(lambert_coo[0][0])},{int(lambert_coo[0][1])},')
                 if int(b_list[0]) <= int(lambert_coo[0][0]) <= int(b_list[2]) and int(b_list[1]) <= int(lambert_coo[0][1]) <= int(b_list[3]) :
                     ds_dsm = possible_dsm_ds
-                    self.tif_dsm_clipping(ds,lambert_coo)
-    
-    def tif_dtm(self, lambert_coo):
-            folder_path = os.path.abspath("/home/becode2/data/GeoTIFF_DTM/")
-            for path, dirs, files in os.walk(folder_path):
-                for filename in files:
-                    tif = f'{folder_path}/{filename}'
-                    possible_dtm_ds = rasterio.open(tif)
-                    b_list = possible_dtm_ds.bounds
-                    #print(f'{possible_ds}, {int(b_list[0])},{int(b_list[1])},{int(lambert_coo[0][0])},{int(lambert_coo[0][1])},')
-                    if int(b_list[0]) <= int(lambert_coo[0][0]) <= int(b_list[2]) and int(b_list[1]) <= int(lambert_coo[0][1]) <= int(b_list[3]) :
-                        ds_dtm = possible_dtm_ds
-                        self.tif_dsm_clipping(ds,lambert_coo)
-
-    def boundries(self, )
-         xmin = lambert_coo[0][0] - 50
-        xmax = lambert_coo[0][0] + 50
-        ymin = lambert_coo[0][1] - 50
-        ymax = lambert_coo[0][1] + 50
-        bbox = (xmin,ymax,xmax,ymin)           
-
-    def tif_dsm_clipping(self,ds_dsm,lambert_coo):
-        
-        print(bbox, lambert_coo[0][1])
-        gdal.Translate('new.tif', ds.name, projWin = bbox)
-        ds2 = rasterio.open('new.tif')
-        self.final_plot(ds2)
-
-    
+                    tif_dtm = f'{folder_path.replace("DSM","DTM")}/{filename.replace("DSM","DTM")}'
+                    ds_dtm = rasterio.open(tif_dtm)
+                    #print(ds_dsm, ds_dtm)
+                    self.tif_clipping(ds_dsm, ds_dtm)
                     
 
-    def tif_dtm_clipping(self,ds,lambert_coo):
-        xmin = lambert_coo[0][0] - 50
-        xmax = lambert_coo[0][0] + 50
-        ymin = lambert_coo[0][1] - 50
-        ymax = lambert_coo[0][1] + 50
-        bbox = (xmin,ymax,xmax,ymin)
-        print(bbox, lambert_coo[0][1])
-        gdal.Translate('new.tif', ds.name, projWin = bbox)
-        ds2 = rasterio.open('new.tif')
-        self.final_plot(ds2)
 
-    def final_plot(self,ds3):
-        array = ds3.read(1)
-        df = pd.DataFrame(data=array)
-        fig = go.Figure(data=[go.Surface(z=df.values)])
-        fig.update_layout(title='Pyiterator', autosize=False, width=1000, height=1000, margin=dict(l=65, r=50, b=65, t=90))
+    def tif_clipping(self, ds_dsm, ds_dtm):
+        gdal.Translate('new_dsm.tif', ds_dsm.name, projWin = self.boundries())
+        new_dsm = rasterio.open('new_dsm.tif')
+        gdal.Translate('new_dtm.tif', ds_dtm.name, projWin = self.boundries())
+        new_dtm = rasterio.open('new_dtm.tif')
+        print(ds_dsm.name, ds_dtm.name)
+        self.chm(new_dtm, new_dsm)
+
+    def chm(self, new_dtm, new_dsm):
+        ds_dsm_read = new_dsm.read(1)
+        ds_dtm_read = new_dtm.read(1)
+        ds_chm =   ds_dsm_read - ds_dtm_read
+        self.final_plot(ds_chm)
+
+    def final_plot(self,ds_chm):
+        fig = go.Figure(data=[go.Surface(z=ds_chm)])
         fig.show()
-        ds3.close()
+        
 Search()
